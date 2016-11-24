fd88462518d0e23a501107044ce6389db01e98a9
ti2016 <m15035023129@163.com>
2016-11-24 15:33:33 +0800

    dsfdddfd

---
+++ app/src/main/java/ti/git3/MainActivity.java
@@ -0,0 +1,17 @@
+package ti.git3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //fasdfadsfa
+        Toast.makeText(this, "hehe", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "hehe", Toast.LENGTH_SHORT).show();
+    }
+}
